867e12b37d27cfadea650dee0f406032134b6f8f
LeeByeongMuk <mansogod@gmail.com>
2024-06-11 21:56:45 +0900

    Add baekjoon 10818

---
+++ baekjoon/step4/10818.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+
+const arr = input[1].split(' ').map(Number);
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+
+console.log(min, max);

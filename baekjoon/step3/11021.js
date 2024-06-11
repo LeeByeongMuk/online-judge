271f57a4b617099c7abe7a8b1066adc1cace153f
LeeByeongMuk <mansogod@gmail.com>
2024-06-11 11:29:49 +0900

    Add baekjoon 11021

---
+++ baekjoon/step3/11021.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const [_, ...arr] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+
+const result = arr.map((s, i) => {
+  const [a, b] = s.split(' ');
+  const sum = parseInt(a) + parseInt(b);
+  return `Case #${i + 1}: ${sum}`
+});
+console.log(result.join('\n'));

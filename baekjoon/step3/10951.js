f44d15e5cdd5a27599844aad4705cd440a0843cd
LeeByeongMuk <mansogod@gmail.com>
2024-06-11 11:29:26 +0900

    Add baekjoon 10951

---
+++ baekjoon/step3/10951.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n').map((d) => {
+  const [a, b] = d.split(' ');
+  return parseInt(a) + parseInt(b);
+});
+
+console.log(input.join('\n'));
+
